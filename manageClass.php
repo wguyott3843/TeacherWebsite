e462900d56ffa6f94155cf42b51479323fd4b141
wguyott3843 <wguyott0785@westfield.ma.edu>
2017-03-26 19:39:50 -0400

    Implemented addClass, updateClass, and deleteClass.  Also finished implementing manageClass

--- manageClass.php
+++ manageClass.php
@@ -2,32 +2,64 @@
 	include('session.php');
 	include('classes.php');
    
-	$numberOfClasses = sizeof($classes);
-   
-	if($numberOfClasses > 0){
-		if (isset($_GET['classId'])) {
-			$classId = $_GET['classId'];
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		if(isset($_POST['operation'])){
+			$operation = $_POST['operation'];
+			if($operation == 'add'){
+				$_SESSION['class_number'] = $_POST['classNumber'];
+				$_SESSION['class_name'] = $_POST['className'];
+				$_SESSION['class_description'] = $_POST['classDescription'];
+				header("location:addClass.php");
+			}
+			elseif($operation == 'delete'){
+				header("location:deleteClass.php");
+			}
+			elseif($operation == 'update'){
+				$_SESSION['class_number'] = $_POST['classNumber'];
+				$_SESSION['class_name'] = $_POST['className'];
+				$_SESSION['class_description'] = $_POST['classDescription'];
+				header("location:updateClass.php");
+			}
+			elseif($operation == 'setClassId'){
+				$classId = $_POST['classId'];
+				$_SESSION['class_id'] = $classId;
+				header("location: manageClass.php");
+			}
+			else{
+				header("location:logout.php");
+			}
 		}
 		else{
-			$classId = $classes[0]['ClassId'];
+			header("location:logout.php");
 		}
-		
-		foreach($classes as $class): 
-			if($class['ClassId'] == $classId){
-				$classNumber = $class['Number'];
-				$className = $class['Name'];
-				$classDescription = $class['Description'];
-				break;
-			}
-		endforeach;
 	}
 	else{
-		$classId = -1;
-		$classNumber = "";
-		$className = "";
-		$classDescription = "";
+		$numberOfClasses = sizeof($classes);
+	   
+		if($numberOfClasses > 0){
+			if (isset($_SESSION['class_id'])) {
+				$classId = $_SESSION['class_id'];
+			}
+			else{
+				$classId = $classes[0]['ClassId'];
+			}
+			
+			foreach($classes as $class): 
+				if($class['ClassId'] == $classId){
+					$classNumber = $class['Number'];
+					$className = $class['Name'];
+					$classDescription = $class['Description'];
+					break;
+				}
+			endforeach;
+		}
+		else{
+			$classId = -1;
+			$classNumber = "";
+			$className = "";
+			$classDescription = "";
+		}
 	}
-
 ?>
 
 <html lang = "en">
@@ -50,11 +82,12 @@
   </head>
   <body>
     <div class = "container">
-		<form name = "selectClassForm" id = "selectClassForm" method = "get">
+		<form name = "selectClassForm" id = "selectClassForm" method = "post">
 			<div class = "form-group">
 				<input class = "span2" id = "classId" name = "classId" type = "hidden">
+				<input class = "span2" id = "operation" name = "operation" type = "hidden">
 				<label for = "classList">Select list (select one):</label>
-					<select class = "form-control" id = "classList" onchange = "window.location='editClass.php?classId='+this.value">
+					<select class = "form-control" id = "classList" onchange = "SetClassIdAndPost(this.value)">
 						<?php foreach($classes as $class): ?>
 							<option value = <?php echo $class['ClassId'] ?> <?php if($class['ClassId'] == $classId){echo "selected";}?>><?php echo $class['Name']; ?></option> 
 						<?php endforeach;?>
@@ -75,9 +108,9 @@
 		<div style = "float:right">
 		<h3>Manage Class:</h3>
 			<ul style = "list-style-type: none">
-				<li><a class = "btn" href = "addClass.php" type = "button">add</a></li>
-				<li><a class = "btn" href = "deleteClass.php" type = "button">delete</a></li>
-				<li><a class = "btn" href = "updateClass.php" type = "button">update</a></li>
+				<li><button onclick = "SetOperationAndPost('add')">add</button></li>
+				<li><button onclick = "SetOperationAndPost('delete')">delete</button></li>
+				<li><button onclick = "SetOperationAndPost('update')">update</button></li>
 			</ul>
 		</div>
 		<div style = "float:right">
@@ -93,6 +126,18 @@
 	
 	<!-- Put all javascript at the end of the body so the UI elements get rendered first.
 		 This makes the webpage seem more responsive to the user. -->
+	<script>
+		function SetClassIdAndPost(classId) {
+			document.getElementById("operation").value = 'setClassId';
+			document.getElementById("classId").value = classId;
+			document.selectClassForm.submit();
+		}
+		
+		function SetOperationAndPost(operation){
+			document.getElementById("operation").value = operation;
+			document.selectClassForm.submit();
+		}
+	</script>
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src = "bootstrap/js/bootstrap.min.js"></script>
   </body>
